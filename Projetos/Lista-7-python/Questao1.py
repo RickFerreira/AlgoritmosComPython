a5a26788bc5d053c1aee2478e5179bc8877283df
Richard Ferreira Salviano <salviano.richard@academico.ifpb.edu.br>
2023-10-09 21:57:27 -0300

    Add files via upload

---
+++ Projetos/Lista-7-python/Questao1.py
@@ -0,0 +1,2 @@
+numero = int(input("Digite um número: "))
+print('O número é {}.'.format('positivo' if numero >= 0 else 'negativo'))
---
+++ Projetos/Lista-7-python/main.py
@@ -0,0 +1,6 @@
+# Autor: Richard Ferreira Salviano
+# Lista 7 - Expressão condicional
+
+# Arquivo utilizado para testar os códigos
+# Você pode copiar e colar qualquer questão aqui
+# Caso vá executar o código no replit, por exemplo
